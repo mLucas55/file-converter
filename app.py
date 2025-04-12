5acbd7b0f13a56760e58502474f373c454e52d10
janellewatwit <janellew@wit.edu>
2025-04-12 00:33:31 -0400

    added file transcoding with ffmpeg

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, abort, flash
+from flask import Flask, render_template, request, abort, flash, send_file
 from werkzeug.utils import secure_filename
+import ffmpeg
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.webp', '.avif', '.mp4']
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.webp', '.avif']
 app.config['UPLOAD_PATH'] = 'uploads'
 
 @app.route('/')
@@ -24,6 +25,20 @@ def upload_files():
         file_ext = os.path.splitext(filename)[1]
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
             abort(400)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        # save uploaded file
+        upload_path = os.path.join(app.config['UPLOAD_PATH'], filename)
+        uploaded_file.save(upload_path)
+
+        # construct output file path
+        output_path = os.path.basename(upload_path)
+        output_path = os.path.splitext(output_path)[0]
+        output_path = output_path + '.' + request.form["output-type"]
+        output_path = os.path.join("static", "output", output_path)
+
+        # transcode file
+        stream = ffmpeg.input(upload_path)
+        stream = ffmpeg.output(stream, output_path)
+        ffmpeg.run(stream, quiet=True, overwrite_output=True)
+
     flash('Upload Successful!', 'success')
-    return redirect(url_for('index'))
+    return send_file(output_path, as_attachment=True)
